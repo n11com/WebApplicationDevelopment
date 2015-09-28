0a899852fefcf76a31d02879d60e8b559d469368
Yusuf Soysal <yusuf.soysal@n11.com>
2015-09-29 00:11:29 +0300

    Rest samples

--- week3/reservation/src/main/java/com/yusuf/boot/reservation/ReservationRepository.java
+++ week3/reservation/src/main/java/com/yusuf/boot/reservation/ReservationRepository.java
@@ -1,11 +1,16 @@
 package com.yusuf.boot.reservation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Collection;
 
+@RepositoryRestResource
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    Collection<Reservation> findByReservationName(String reservationName);
+    @RestResource(path="by-name" )
+    Collection<Reservation> findByReservationName(@Param("rn") String reservationName);
 
 }
